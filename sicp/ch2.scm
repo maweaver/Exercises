8b394f44b341bbadfcaa808c29f3bf2235f14cd2
Matt Weaver <matt@mac04.federation.circuitvision.com>
2009-07-20 11:21:56 -0400

    2.19

--- sicp/ch2.scm
+++ sicp/ch2.scm
@@ -426,5 +426,26 @@
 			(reverse-step (cdr l) (cons (car l) reversed))))
 	(reverse-step l nil))
 
-(display (reverse (list 1 4 9 16 25)))
+;(display (reverse (list 1 4 9 16 25)))
+;(newline)
+
+;;; 2.19 ;;;
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+	(define (no-more? coin-values) (null? coin-values))
+	(define (first-denomination coin-values) (car coin-values))
+	(define (except-first-denomination coin-values) (cdr coin-values))
+	(cond ((= amount 0) 1)
+		((or (< amount 0) (no-more? coin-values)) 0)
+		(else
+			(+ (cc amount
+					(except-first-denomination coin-values))
+				(cc (- amount
+						(first-denomination coin-values))
+					coin-values)))))
+
+(display (cc 100 us-coins))
 (newline)
